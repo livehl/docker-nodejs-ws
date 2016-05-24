036b7dbc7700782a99649b01d716ba862bf559ee
Linhua Tan <toolchainx@gmail.com>
2016-05-24 17:08:50 +0800

    Update server.js

--- build/app/server.js
+++ build/app/server.js
@@ -1,38 +1,46 @@
-// Source: https://github.com/einaros/ws
- 
-var WebSocketServer = require('ws').Server
-, wss = new WebSocketServer({ port: 8080 });
- 
-wss.on('connection', function connection(ws) 
-{
-   ws.on('message', function incoming(message) 
-   {
-      console.log('received: %s', message);
-   });
- 
-   ws.send('something');
-}); 
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
 
-// static webserver
-// Source: https://www.npmjs.com/package/node-static
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
 
-var static = require('node-static');
- 
-var fileServer = new static.Server('/root');
- 
-require('http').createServer(function (request, response) 
-{
-    request.addListener('end', function () 
-    {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(80);
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
 
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
 
-console.log('Listening on port 80.');
-
-
-
-
-
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
