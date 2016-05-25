6c6fbd89405dfa004c42a1c37fb21b9122050349
linhua.tlh <linhua.tlh@alibaba-inc.com>
2016-05-25 15:00:35 +0800

    fix

--- build/app/server.js
+++ build/app/server.js
@@ -7,7 +7,7 @@ wss.on('connection', function connection(ws)
 {
    ws.on('message', function incoming(message) 
    {
-      console.log('received: %s', message);
+      console.log('received: %s', message.data);
    });
  
    ws.send('something');
